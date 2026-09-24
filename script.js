56b5ae0470a0682737e09b5d94e5b8b7d7810094
Anderson <anderson99624765@gmail.com>
2026-09-23 22:47:34 -0300

    rolagem

--- script.js
+++ script.js
@@ -1014,11 +1014,7 @@ function monitorarStatusLoja() {
             if (estaAberta) {
                 overlay.style.display = 'none';
                 document.body.style.overflow = '';
-            } else {
-<<<<<<< HEAD
                 // Notifica que está fechado, mas não trava navegação após fechar
-=======
->>>>>>> 72a6ede (adicionado sistema de identidade visual com opções de personalização e ajustes de estilo)
                 overlay.style.display = 'flex';
             }
         }
